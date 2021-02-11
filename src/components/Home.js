b1b9110faf48949bb723e4551cbdb86a5873d4b7
Joanne Basa <joannegbasa@gmail.com>
2021-02-11 22:33:03 +0000

    change home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
  
   return (
     <div className="home">
-      <Link className="home-link" to="/nasaphoto" >See into the stars!</Link>
+      <Link className="home-link" to="/nasaphoto" >Hello, astronomer!</Link>
     </div>
   );
 }
